0b1d969545c4febd09477a314aee4e8475861b3e
Mohammad Hammoud <mohammadhammoud1921@gmail.com>
2019-04-25 15:27:37 +0300

    Update index.php

--- best temp ever/index.php
+++ best temp ever/index.php
@@ -30,8 +30,7 @@
     <!-- THE LOADER -->
     <div class="be-loader">
         <div class="spinner">
-            <img src="img/logo-loader.png" alt="">
-            <p class="circle">
+            <img src="img/text.png" alt="">            <p class="circle">
                 <span class="ouro">
                     <span class="left"><span class="anim"></span></span>
                     <span class="right"><span class="anim"></span></span>
