c20d021fe9f53a18bedc58cf9a7306f40b92b19d
hadihaidar <hadihaidar@users.noreply.github.com>
2019-04-29 16:27:02 +0300

    searching is done just need to change the  path

--- best temp ever/search.php
+++ best temp ever/search.php
@@ -2,6 +2,8 @@
 if (!isset($_SESSION['name'])) {
 	header("location:index.php");
 }
+
+
 ?>
 <html>
 
@@ -85,10 +87,10 @@ if (!isset($_SESSION['name'])) {
 		<div class="container-fluid custom-container">
 			<div class="row">
 				<div class="col-md-2 left-feild">
-					<form action="search.php" class="input-search">
-						<input type="text" required placeholder="Enter keyword">
+					<form action="search.php" method="POST" class="input-search">
+						<input type="text" required placeholder="Enter keyword" name='name'>
 						<i class="fa fa-search"></i>
-						<input type="submit" value="">
+						<input type="submit" name="search" value="">
 					</form>
 				</div>
 				<div class="col-md-10 ">
@@ -114,341 +116,39 @@ if (!isset($_SESSION['name'])) {
 				</div>
 			</div>
 		</div>
-
 		<div class="container-fluid custom-container">
 			<div class="row">
 				<div class="col-md-12">
 					<div id="container-mix" class="row _post-container_">
-						<div class="category-1 mix custom-column-5">
-							<div class="be-post">
-								<a href="page1.html" class="be-img-block">
-									<img src="img/p1.jpg" alt="omg">
-								</a>
-								<a href="page1.html" class="be-post-title">The kitsch destruction of our world</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a1.png" alt="" class="ava-author">
-									<span>by <a href="page1.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-2 mix custom-column-5">
-							<div class="be-post">
-								<a href="page2.html" class="be-img-block">
-									<img src="img/p2.jpg" alt="omg">
-								</a>
-								<a href="page2.html" class="be-post-title">Treebeard</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a1.png" alt="" class="ava-author">
-									<span>by <a href="page2.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-3 mix custom-column-5">
-							<div class="be-post">
-								<a href="page3.html" class="be-img-block">
-									<img src="img/p3.jpg" alt="omg">
-								</a>
-								<a href="page3.html" class="be-post-title">Colors of Ramadan</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a2.png" alt="" class="ava-author">
-									<span>by <a href="page3.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-4 mix custom-column-5">
-							<div class="be-post">
-								<a href="page1.html" class="be-img-block">
-									<img src="img/p4.jpg" alt="omg">
-								</a>
-								<a href="page1.html" class="be-post-title">Leaving Home - L'Officiel Ukraine</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a3.png" alt="" class="ava-author">
-									<span>by <a href="page1.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-5 mix custom-column-5">
-							<div class="be-post">
-								<a href="page2.html" class="be-img-block">
-									<img src="img/p5.jpg" alt="omg">
-								</a>
-								<a href="page2.html" class="be-post-title">Drive Your World</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a4.png" alt="" class="ava-author">
-									<span>by <a href="page2.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-6 mix custom-column-5">
-							<div class="be-post">
-								<a href="page1.html" class="be-img-block">
-									<img src="img/p13.jpg" alt="omg">
-								</a>
-								<a href="page1.html" class="be-post-title">Fran Ewald for The Diaries Project</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a5.png" alt="" class="ava-author">
-									<span>by <a href="page1.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-5 mix custom-column-5">
-							<div class="be-post">
-								<a href="page1.html" class="be-img-block">
-									<img src="img/p7.jpg" alt="omg">
-								</a>
-								<a href="page1.html" class="be-post-title">raindrops monochrome</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a6.png" alt="" class="ava-author">
-									<span>by <a href="page1.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-3 mix custom-column-5">
-							<div class="be-post">
-								<a href="page3.html" class="be-img-block">
-									<img src="img/p8.jpg" alt="omg">
-								</a>
-								<a href="page3.html" class="be-post-title">Racing Queensland</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a7.png" alt="" class="ava-author">
-									<span>by <a href="page3.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-2 mix custom-column-5">
-							<div class="be-post">
-								<a href="page1.html" class="be-img-block">
-									<img src="img/p9.jpg" alt="omg">
-								</a>
-								<a href="page1.html" class="be-post-title">NAHA Finalist Hairstylist of the Year Allen Ruiz</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a7.png" alt="" class="ava-author">
-									<span>by <a href="page1.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-4 mix custom-column-5">
-							<div class="be-post">
-								<a href="page1.html" class="be-img-block">
-									<img src="img/p10.jpg" alt="omg">
-								</a>
-								<a href="page1.html" class="be-post-title">tomorrow</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a8.png" alt="" class="ava-author">
-									<span>by <a href="page1.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-6 mix custom-column-5">
-							<div class="be-post">
-								<a href="page1.html" class="be-img-block">
-									<img src="img/p11.jpg" alt="omg">
-								</a>
-								<a href="page1.html" class="be-post-title">Tropicalia</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a5.png" alt="" class="ava-author">
-									<span>by <a href="page1.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-2 mix custom-column-5">
-							<div class="be-post">
-								<a href="page1.html" class="be-img-block">
-									<img src="img/p12.jpg" alt="omg">
-								</a>
-								<a href="page1.html" class="be-post-title">Face</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a6.png" alt="" class="ava-author">
-									<span>by <a href="page1.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-1 mix custom-column-5">
-							<div class="be-post">
-								<a href="page1.html" class="be-img-block">
-									<img src="img/p13.jpg" alt="omg">
-								</a>
-								<a href="page1.html" class="be-post-title">Stay Ahead Series</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a5.png" alt="" class="ava-author">
-									<span>by <a href="page1.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-3 mix custom-column-5">
-							<div class="be-post">
-								<a href="page1.html" class="be-img-block">
-									<img src="img/p14.jpg" alt="omg">
-								</a>
-								<a href="page1.html" class="be-post-title">Faber-Castell / Psychological Problems</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a1.png" alt="" class="ava-author">
-									<span>by <a href="page1.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
-						<div class="category-1 mix custom-column-5">
-							<div class="be-post">
-								<a href="page1.html" class="be-img-block">
-									<img src="img/p15.jpg" alt="omg">
-								</a>
-								<a href="page1.html" class="be-post-title">Garry Simpson - Bridges - Intelligent Life Magazine</a>
-								<span>
-									<a href="blog-detail-2.html" class="be-post-tag">Interaction Design</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">UI/UX</a>,
-									<a href="blog-detail-2.html" class="be-post-tag">Web Design</a>
-								</span>
-								<div class="author-post">
-									<img src="img/a9.png" alt="" class="ava-author">
-									<span>by <a href="page1.html">Hoang Nguyen</a></span>
-								</div>
-								<div class="info-block">
-									<span><i class="fa fa-thumbs-o-up"></i> 360</span>
-									<span><i class="fa fa-eye"></i> 789</span>
-									<span><i class="fa fa-comment-o"></i> 20</span>
-								</div>
-							</div>
-						</div>
+						<?php
+						if (isset($_POST['search'])) {
+							$db = new PDO("mysql:dbname=thedoctors", "root", "");
+							$name = strtolower($_POST['name']) . " !@#$%^&&";
+							$name = explode(" ", $name);
+							$query = $db->query("SELECT * FROM user ");
+							$COUNT = 0;
+							foreach ($query as $row) {
+								if (strtolower($row['FirstName']) == $name[0] || strtolower($row['LastName']) == $name[1] || strtolower($row['FirstName']) == $name[1] || strtolower($row['LastName']) == $name[0]) {
+									$COUNT = 1;
+									?>
+									<div class="category-1 mix custom-column-5">
+										<div class="be-post">
+											<a href="page1.html" class="be-img-block">
+												<img src="img/p1.jpg" alt="omg">
+											</a>
+											<div class="author-post" style = "font-size: 16px;" >
+												<span><a  href="page1.html"><b><?php echo($row['FirstName']." ".$row['LastName']);?></b></a></span>
+											</div>
+										
+										</div>
+									</div>
+								<?php
+							}
+						}
+					}
+
+					?>
+
 					</div>
 				</div>
 
